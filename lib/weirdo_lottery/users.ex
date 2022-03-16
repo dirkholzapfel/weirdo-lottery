c46f6b23971cdbc9277984d84f5779ae9f80a5d9
Dirk Holzapfel <dirk@steadyHQ.com>
2022-03-16 12:52:42 +0100

    Add draw_winners functionality
    The draw_winners function in the Users context randomly draws up to 2 users that are eligible to win.

--- lib/weirdo_lottery/users.ex
+++ lib/weirdo_lottery/users.ex
@@ -101,4 +101,30 @@ defmodule WeirdoLottery.Users do
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
   end
+
+  @spec draw_winners(integer()) :: list(User.t())
+  @doc """
+  Returns a list with up to 2 winning `User{}` elements.
+
+  A user is a potential winner if they have more `points` than
+  the given `points_threshold`.
+
+  The winners are drawn in a random way.
+
+  ## Examples
+
+      iex> draw_winners(23)
+      [%User{}, %User{}]
+
+  """
+  def draw_winners(points_threshold \\ 0) do
+    query =
+      from u in User,
+        where: u.points > ^points_threshold,
+        order_by: fragment("RANDOM()"),
+        select: [:id, :points],
+        limit: 2
+
+    Repo.all(query)
+  end
 end
